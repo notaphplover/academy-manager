90a21848abc276c2171df9cd8e2f3c3b61931c2d
notaphplover <roberto.pintos.lopez@gmail.com>
2025-09-26 14:49:05 +0200

    feat: add backend-common package

---
+++ packages/backend/libraries/common/README.md
@@ -0,0 +1,6 @@
+[![Test coverage](https://codecov.io/gh//notaphplover/academy-manager/branch/main/graph/badge.svg?flag=%40academyjs%2Fbackend-common)](https://codecov.io/gh//notaphplover/academy-manager/branch/main/graph/badge.svg?flag=%40academyjs%2Fbackend-common)
+[![npm version](https://img.shields.io/github/package-json/v//notaphplover/academy-manager?filename=packages%2Fbackend%2Fauth%2Fbetter-auth-adapter%2Fpackage.json&style=plastic)](https://www.npmjs.com/package/@academyjs/backend-common)
+
+# @academyjs/backend-common
+
+Inversify monorepo backend common package.
---
+++ packages/backend/libraries/common/src/index.ts
@@ -0,0 +1,29 @@
+import { Writable } from './common/application/models/Writable';
+import { Handler } from './common/application/modules/Handler';
+import { Builder } from './common/domain/modules/Builder';
+import { BuilderAsync } from './common/domain/modules/BuilderAsync';
+import { ReportBasedSpec } from './common/domain/modules/ReportBasedSpec';
+import { Spec } from './common/domain/modules/Spec';
+import {
+  BaseEither,
+  Either,
+  Left,
+  Right,
+} from './common/domain/patterns/fp/Either';
+import { AppError } from './error/application/models/AppError';
+import { AppErrorKind } from './error/application/models/AppErrorKind';
+
+export { AppError, AppErrorKind };
+
+export type {
+  BaseEither,
+  Builder,
+  BuilderAsync,
+  Either,
+  Handler,
+  Left,
+  ReportBasedSpec,
+  Right,
+  Spec,
+  Writable,
+};
